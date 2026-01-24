bfa1febb2676e0bf8fc0251022dd95acd150a60e
Naikaku <naikaku00@gmail.com>
2026-01-24 18:19:37 +0900

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONFIG_DEFAULT = {
+  text: "",
+  fontSize: 60,
+  textColor: "#ffffff",
+  strokeColor: "#000000",
+  bgColor: "#000000",
+  useBg: false,
+  strokeWidth: 8,
+  baseWidth: 1200,
+  scaleFactor: 2.0,
+  textAlign: "left",
+  bgImage: null,
+  bgOpacity: 0.5
+};
